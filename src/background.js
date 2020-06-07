5c2c84981e3d4139ac526f9a574ddb69e2425781
Steve <peviroy@outlook.com>
2020-06-08 01:11:28 +0800

    Initial vue-electron project

---
+++ src/background.js
@@ -0,0 +1,117 @@
+/* eslint-disable max-len */
+import {
+  app, protocol, BrowserWindow, globalShortcut,
+} from 'electron';
+
+import {
+  createProtocol,
+  /* installVueDevtools */
+} from 'vue-cli-plugin-electron-builder/lib';
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+// Scheme must be registered before the app is ready
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'app', privileges: { secure: true, standard: true } },
+]);
+
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1200,
+    height: 700,
+    center: true, // appear in the center
+    resizable: true,
+    frame: true, // 窗口边界, set true temporay;
+    webPreferences: {
+      webSecurity: false,
+      // Use pluginOptions.nodeIntegration, leave this alone
+      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+    },
+    // eslint-disable-next-line ,no-undef
+    icon: process.platform === 'win32' ? `${__static}/app.ico` : `${__static}/app.png`, // .ico for windows; .png for all
+  });
+
+  // Load URL -> of render process
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+    if (!process.env.IS_TEST) {
+      win.webContents.openDevTools();
+    }
+  } else {
+    createProtocol('app');
+    // Load the index.html when not in development
+    win.loadURL('app://./index.html');
+  }
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    // Install Vue Devtools
+    // Devtools extensions are broken in Electron 6.0.0 and greater
+    // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
+    // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
+    // If you are not using Windows 10 dark mode, you may uncomment these lines
+    // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
+    // try {
+    //   await installVueDevtools()
+    // } catch (e) {
+    //   console.error('Vue Devtools failed to install:', e.toString())
+    // }
+  }
+  globalShortcut.register('CommandOrControl+Shift+i', () => {
+    win.webContents.openDevTools();
+  });
+  createWindow();
+});
+// Exit cleanly on request from parent process in development mode.
+if (isDevelopment) {
+  if (process.platform === 'win32') {
+    process.on('message', (data) => {
+      if (data === 'graceful-exit') {
+        app.quit();
+      }
+    });
+  } else {
+    process.on('SIGTERM', () => {
+      app.quit();
+    });
+  }
+}
+
+// will-finish-launching 在应用完成基本启动进程之后触发
+// ready 当electron完成初始化后触发; register initial command
+// window-all-closed 所有窗口都关闭的时候触发，在windows和linux里，所有窗口都退出的时候通常是应用退出的时候
+// before-quit 退出应用之前的时候触发
+// will-quit 即将退出应用的时候触发; unregister
+// quit 应用退出的时候触发
